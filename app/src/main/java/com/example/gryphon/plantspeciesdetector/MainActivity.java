235c1346791f7c3a4790ce35fb37c28588bd0e98
Hoang Duc Quan <rocker.gryphon@gmail.com>
2018-08-10 13:51:58 +0700

    add take picture

--- app/src/main/java/com/example/gryphon/plantspeciesdetector/MainActivity.java
+++ app/src/main/java/com/example/gryphon/plantspeciesdetector/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.gryphon.plantspeciesdetector;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnCamera;
+    private ImageView capturedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
+        btnCamera = (Button)findViewById( R.id.camera );
+        btnCamera.setTypeface(font);
+        capturedImage= (ImageView) findViewById(R.id.capturedImage);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK) {
+            Bitmap bp = (Bitmap) data.getExtras().get("data");
+            capturedImage.setImageBitmap(bp);
+        }
     }
 }
